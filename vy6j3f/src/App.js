7a685488b20b5bbd5826cfecce105f4560ca612c
loifaith <146465965+loifaith@users.noreply.github.com>
2023-10-16 02:30:00 +0900

    final version

--- vy6j3f/src/App.js
+++ vy6j3f/src/App.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
+
+
 export default function board() {
+  const value = useState([Array(9).full(null)])
+
   return (
     <>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
+      <div className="board-row">
+        <Square value={value[0]}></Square>
+        <Square value={value[1]}></Square>
+        <Square value={value[2]}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={value[3]}></Square>
+        <Square value={value[4]}></Square>
+        <Square value={value[5]}></Square>
+      </div>
+      <div className="board-row">
+        <Square value={value[6]}></Square>
+        <Square value={value[7]}></Square>
+        <Square value={value[8]}></Square>
+      </div>
     </>
   )
 
@@ -17,6 +28,10 @@ export default function board() {
 
 
 
-function Square() {
-  return <button className="square"></button>;
+function Square({value}) {
+  function onClickSqure() {
+    alert("Square Clicked!!!")
+  }
+
+  return <button className="square" onClick={onClickSqure()}>{value}</button>;
 }
